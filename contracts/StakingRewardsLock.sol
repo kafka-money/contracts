5bdbcdbfb16a6aa9389a6da219f3f5f997893a45
kafka <KAFKA-FINANCE@protonmail.com@>
2021-11-27 18:46:33 +0800

    stake reward lock

---
+++ contracts/StakingRewardsLock.sol
@@ -0,0 +1,313 @@
+/**
+ *Submitted for verification at Etherscan.io on 2020-09-16
+ */
+
+pragma solidity =0.6.6;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+import "./test/fmt.sol";
+
+// Inheritancea
+// Inheritancea
+interface IStakingRewards {
+    // Views
+    function lastTimeRewardApplicable() external view returns (uint256);
+
+    function rewardPerToken() external view returns (uint256);
+
+    function earned(address account) external view returns (uint256);
+
+    function getRewardForDuration() external view returns (uint256);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    // Mutative
+
+    function stake(uint256 amount) external;
+
+    function withdraw(uint256 amount) external;
+
+    function getReward() external;
+
+    function exit() external;
+}
+
+//contract StakingRewards is IStakingRewards, RewardsDistributionRecipient, ReentrancyGuard {
+contract StakingRewardsLock is ReentrancyGuard, Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    /* ========== STATE VARIABLES ========== */
+
+    IERC20 public rewardsToken;
+    IERC20 public stakingToken;
+    uint256 public periodFinish = 0;
+    uint256 public rewardRate = 0;
+    uint256 public rewardsDuration = 90 days;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards; // in locked
+    mapping(address => uint256) public rewardsTime; // used to calculate lock-up
+    uint256 public constant LOCK_PERIOD = 90 days;
+    mapping(address => uint256) public unlocked; // un locked
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+    uint256 public immutable CREATED_TIME;
+
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor(address _rewardsToken, address _stakingToken) public {
+        rewardsToken = IERC20(_rewardsToken);
+        stakingToken = IERC20(_stakingToken);
+        CREATED_TIME = block.timestamp;
+    }
+
+    function earlyPenalty(uint256 amount)
+        public
+        pure
+        returns (uint256 eraly, uint256 penalty)
+    {
+        eraly = amount / 3;
+        penalty = amount - eraly;
+    }
+
+    function curEpochStart() public view returns (uint256) {
+        uint256 epoch = lastTimeRewardApplicable().sub(CREATED_TIME).div(
+            LOCK_PERIOD
+        );
+        return CREATED_TIME.add(epoch.mul(LOCK_PERIOD));
+    }
+
+    /* ========== VIEWS ========== */
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        if (periodFinish == 0) return block.timestamp;
+        return Math.min(block.timestamp, periodFinish);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (_totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored.add(
+                lastTimeRewardApplicable()
+                    .sub(lastUpdateTime)
+                    .mul(rewardRate)
+                    .mul(1e18)
+                    .div(_totalSupply)
+            );
+    }
+
+    function deltaEarned(address account) public view returns (uint256) {
+        return
+            _balances[account]
+                .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+                .div(1e18);
+    }
+
+    function earned(address account) external view returns (uint256) {
+        return deltaEarned(account).add(rewards[account]).add(unlocked[account]);
+    }
+
+    function getRewardForDuration() external view returns (uint256) {
+        return rewardRate.mul(rewardsDuration);
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    function stakeWithPermit(
+        uint256 amount,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external nonReentrant updateReward(msg.sender) {
+        require(amount > 0, "Cannot stake 0");
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+
+        // permit
+        IUniswapV2ERC20(address(stakingToken)).permit(
+            msg.sender,
+            address(this),
+            amount,
+            deadline,
+            v,
+            r,
+            s
+        );
+
+        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function stake(uint256 amount)
+        external
+        nonReentrant
+        updateReward(msg.sender)
+    {
+        require(amount > 0, "Cannot stake 0");
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount)
+        public
+        nonReentrant
+        updateReward(msg.sender)
+    {
+        _withdraw(amount);
+    }
+
+    // emergency withdraw
+    function withdrawOnly(uint256 amount) public nonReentrant {
+        _withdraw(amount);
+    }
+
+    function _withdraw(uint256 amount) private {
+        require(amount > 0, "Cannot withdraw 0");
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        stakingToken.safeTransfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function getReward(bool force)
+        public
+        nonReentrant
+        updateReward(msg.sender)
+    {
+        uint256 freed = unlocked[msg.sender];
+        unlocked[msg.sender] = 0;
+        rewardsToken.safeTransfer(msg.sender, freed);
+        emit RewardPaid(msg.sender, freed);
+        if (force) {
+            (uint256 early, uint256 penalty) = earlyPenalty(
+                rewards[msg.sender]
+            );
+            rewards[msg.sender] = 0;
+            rewardsTime[msg.sender] = block.timestamp;
+            rewardsToken.safeTransfer(msg.sender, early);
+            emit RewardPaid(msg.sender, early);
+            uint256 burned = penalty / 6;
+            uint256 notified = penalty - burned;
+            rewardsToken.safeTransfer(address(0), burned);
+            emit EarlyPenalty(msg.sender, early, burned, address(0), notified);
+        }
+    }
+
+    function exit() external {
+        withdraw(_balances[msg.sender]);
+        getReward(true);
+    }
+
+    /* ========== RESTRICTED FUNCTIONS ========== */
+    function setRewardRate(uint256 rewardPerSecond)
+        external
+        onlyOwner
+        updateReward(address(0))
+    {
+        emit SetRewardRate(rewardRate, rewardPerSecond);
+        rewardRate = rewardPerSecond;
+    }
+
+    function setPeriodFinish(uint256 _periodFinish) external onlyOwner {
+        periodFinish = _periodFinish;
+    }
+
+    /* ========== MODIFIERS ========== */
+
+    function earnedDetails(address account)
+        public
+        view
+        returns (
+            uint256 rewardTime,
+            uint256 _unlocked,
+            uint256 locked
+        )
+    {
+        uint256 deltaReward = deltaEarned(account);
+        uint256 timestamp = lastTimeRewardApplicable();
+        uint256 _curEpochStart = curEpochStart();
+        uint256 lastEpochRewardsTime = rewardsTime[account];
+        _unlocked = unlocked[account];
+        locked = rewards[account];
+        if (lastEpochRewardsTime == 0) {
+            rewardTime = block.timestamp;
+        } else if (lastEpochRewardsTime < _curEpochStart) {
+            uint256 stakeDuration = timestamp - lastEpochRewardsTime;
+            uint256 deltaUnlocked = deltaReward
+                .mul(_curEpochStart.sub(lastEpochRewardsTime))
+                .div(stakeDuration);
+            _unlocked = _unlocked.add(locked).add(deltaUnlocked); // locked become unlocked, and plus delta
+            locked = deltaReward.sub(deltaUnlocked); // locked is delta locked
+            rewardTime = _curEpochStart;
+        } else {
+            locked = locked.add(deltaReward);
+            rewardTime = Math.max(lastEpochRewardsTime, _curEpochStart);
+        }
+        rewardTime = block.timestamp;
+    }
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            (
+                rewardsTime[account],
+                unlocked[account],
+                rewards[account]
+            ) = earnedDetails(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    /* ========== EVENTS ========== */
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+    event EarlyPenalty(
+        address indexed user,
+        uint256 early,
+        uint256 burned,
+        address toStakePool,
+        uint256 toAmount
+    );
+    event SetRewardRate(uint256 oldRate, uint256 newRate);
+}
+
+interface IUniswapV2ERC20 {
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
---
+++ test/lock.test.js
@@ -0,0 +1,375 @@
+const StakingRewardsLock = artifacts.require("StakingRewardsLock");
+const ERC20 = artifacts.require("ERC20");
+
+const BN = (n, d = 10) => new web3.utils.BN(n, d);
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const EMPTY = '0x0000000000000000000000000000000000000000';
+
+const N18 = n => BN(web3.utils.toWei(String(n)))
+
+const percentMul = (a, b, c) => a.mul(new BN(b)).div(new BN(c));
+
+advanceTime = (time) => {
+    return new Promise((resolve, reject) => {
+        web3.currentProvider.send({
+            jsonrpc: '2.0',
+            method: 'evm_increaseTime',
+            params: [time],
+            id: new Date().getTime()
+        }, (err, result) => {
+            if (err) { return reject(err) }
+            return resolve(result)
+        })
+    })
+}
+
+advanceBlock = () => {
+    return new Promise((resolve, reject) => {
+        web3.currentProvider.send({
+            jsonrpc: '2.0',
+            method: 'evm_mine',
+            id: new Date().getTime()
+        }, (err, result) => {
+            if (err) { return reject(err) }
+            const newBlockHash = web3.eth.getBlock('latest').hash
+
+            return resolve(newBlockHash)
+        })
+    })
+}
+
+function show(x) {
+    let keys = Object.keys(x);
+    if(keys.find(k=>isNaN(Number(k)))){
+        keys = keys.filter(k=>isNaN(Number(k)));
+    }
+    console.log(keys.reduce((t,k)=>(t[k]=x[k].toString(),t),{}));
+}
+
+const days = n=>24*3600*n
+
+const xeq=(a,b)=>assert.equal(a<=b+1&&a>=b-1, true)
+
+contract("Stake test", async accounts => {
+    before(
+        async () => {
+            this.initStake = async()=>{
+                this.stakeToken = await ERC20.new(N18(100000000));
+                this.rewardToken = await ERC20.new(N18(100000000));
+                this.staking = await StakingRewardsLock.new(this.rewardToken.address, this.stakeToken.address);
+                await this.staking.setRewardRate(1);
+                await this.rewardToken.transfer(this.staking.address, N18(24 * 3600));
+            }
+        }
+    );
+
+    it(`test stake - getreward(false) `, async () => {
+        await this.initStake();
+        const stakeToken = this.stakeToken;
+        const rewardToken = this.rewardToken;
+        const staking = this.staking;
+        let day = 0;
+        const incDays = async n => {
+            day += n;
+            await advanceTime(days(n));
+            await advanceBlock();
+            console.log("-----------incDays:", n, day)
+        }
+        await stakeToken.approve(staking.address, N18(10000000000));
+        await staking.stake(N18(1));
+        await incDays(90);
+        {
+            const rewards = await staking.earnedDetails(accounts[0]);
+            show(rewards)
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, 0)
+        }
+        await incDays(30);
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            xeq(unlocked, 0)
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, days(30))
+        }
+        const before = await rewardToken.balanceOf(accounts[0]);
+        console.log("getReward")
+        await staking.getReward(false);
+        const after = await rewardToken.balanceOf(accounts[0]);
+        //await advanceBlock();
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            show({delta: after.sub(before)})
+
+            xeq(unlocked, 0)
+            xeq(rewards._unlocked, 0)
+            xeq(rewards.locked, days(30))
+            xeq(after.sub(before), days(90))
+        }
+        await incDays(90);
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, days(30))
+        }
+    });
+
+    it(`test stake - getreward(true)`, async () => {
+        await this.initStake();
+        const stakeToken = this.stakeToken;
+        const rewardToken = this.rewardToken;
+        const staking = this.staking;
+        let day = 0;
+        const incDays = async n => {
+            day += n;
+            await advanceTime(days(n));
+            await advanceBlock();
+            console.log("-----------incDays:", n, day)
+        }
+        await stakeToken.approve(staking.address, N18(10000000000));
+        await staking.stake(N18(1));
+        await incDays(90);
+        {
+            const rewards = await staking.earnedDetails(accounts[0]);
+            show(rewards)
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, 0)
+        }
+        await incDays(30);
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            xeq(unlocked, 0)
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, days(30))
+        }
+        const before = await rewardToken.balanceOf(accounts[0]);
+        console.log("getReward")
+        await staking.getReward(true);
+        const after = await rewardToken.balanceOf(accounts[0]);
+        //await advanceBlock();
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            show({delta: after.sub(before)})
+
+            xeq(unlocked, 0)
+            xeq(rewards._unlocked, 0)
+            xeq(rewards.locked, days(0))
+            xeq(after.sub(before), days(90)+days(30)/3)
+        }
+        await incDays(90);
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            xeq(rewards._unlocked, days(60))
+            xeq(rewards.locked, days(30))
+        }
+    });
+
+    it(`test stake - stake`, async () => {
+        await this.initStake();
+        const stakeToken = this.stakeToken;
+        const rewardToken = this.rewardToken;
+        const staking = this.staking;
+        let day = 0;
+        const incDays = async n => {
+            day += n;
+            await advanceTime(days(n));
+            await advanceBlock();
+            console.log("-----------incDays:", n, day)
+        }
+        await stakeToken.approve(staking.address, N18(10000000000));
+        await staking.stake(N18(1));
+        await incDays(90);
+        {
+            const rewards = await staking.earnedDetails(accounts[0]);
+            show(rewards)
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, 0)
+        }
+        await incDays(30);
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            xeq(unlocked, 0)
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, days(30))
+        }
+        const before = await rewardToken.balanceOf(accounts[0]);
+        console.log("stake again")
+        await staking.stake(N18(1));
+        const after = await rewardToken.balanceOf(accounts[0]);
+        //await advanceBlock();
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            show({delta: after.sub(before)})
+
+            xeq(unlocked, days(90))
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, days(30))
+            xeq(after.sub(before), 0)
+        }
+        await incDays(90);
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            xeq(rewards._unlocked, days(180))
+            xeq(rewards.locked, days(30))
+        }
+    });
+
+    it(`test stake - withdraw`, async () => {
+        await this.initStake();
+        const stakeToken = this.stakeToken;
+        const rewardToken = this.rewardToken;
+        const staking = this.staking;
+        let day = 0;
+        const incDays = async n => {
+            day += n;
+            await advanceTime(days(n));
+            await advanceBlock();
+            console.log("-----------incDays:", n, day)
+        }
+        await stakeToken.approve(staking.address, N18(10000000000));
+        await staking.stake(N18(1));
+        await incDays(90);
+        {
+            const rewards = await staking.earnedDetails(accounts[0]);
+            show(rewards)
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, 0)
+        }
+        await incDays(30);
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            xeq(unlocked, 0)
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, days(30))
+        }
+        const before = await rewardToken.balanceOf(accounts[0]);
+        console.log("withdraw all")
+        await staking.withdraw(N18(1));
+        const after = await rewardToken.balanceOf(accounts[0]);
+        const stakeAmount = await staking.balanceOf(accounts[0]);
+        //await advanceBlock();
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            show({delta: after.sub(before)})
+            
+            xeq(stakeAmount, 0);
+            xeq(unlocked, days(90))
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, days(30))
+            xeq(after.sub(before), 0)
+        }
+        await incDays(90);
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            xeq(rewards._unlocked, days(120))
+            xeq(rewards.locked, days(0))
+        }
+    });
+
+    it(`test stake - withdraw - stake`, async () => {
+        await this.initStake();
+        const stakeToken = this.stakeToken;
+        const rewardToken = this.rewardToken;
+        const staking = this.staking;
+        let day = 0;
+        const incDays = async n => {
+            day += n;
+            await advanceTime(days(n));
+            await advanceBlock();
+            console.log("-----------incDays:", n, day)
+        }
+        await stakeToken.approve(staking.address, N18(10000000000));
+        await staking.stake(N18(1));
+        await incDays(90);
+        {
+            const rewards = await staking.earnedDetails(accounts[0]);
+            show(rewards)
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, 0)
+        }
+        await incDays(30);
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            xeq(unlocked, 0)
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, days(30))
+        }
+        const before = await rewardToken.balanceOf(accounts[0]);
+        console.log("withdraw all")
+        await staking.withdraw(N18(1));
+        const after = await rewardToken.balanceOf(accounts[0]);
+        const stakeAmount = await staking.balanceOf(accounts[0]);
+        //await advanceBlock();
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            show({delta: after.sub(before)})
+            
+            xeq(stakeAmount, 0);
+            xeq(unlocked, days(90))
+            xeq(rewards._unlocked, days(90))
+            xeq(rewards.locked, days(30))
+            xeq(after.sub(before), 0)
+        }
+        await incDays(90);
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            xeq(rewards._unlocked, days(120))
+            xeq(rewards.locked, days(0))
+        }
+        console.log('stake again');
+        await staking.stake(N18(1));
+        await incDays(90);
+        {
+            const rewards = await staking.earnedDetails(accounts[0])
+            const unlocked = await staking.unlocked(accounts[0]);
+            show(rewards)
+            show({unlocked})
+            xeq(rewards._unlocked, days(180))
+            xeq(rewards.locked, days(30))
+        }
+    });
+})
